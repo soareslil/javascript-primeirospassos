6bbb82e422521fb37255fd2cb9ca93976898336d
Liliane Soares <78764415+soareslil@users.noreply.github.com>
2022-03-30 15:19:22 -0300

    trabalhando com muitos elementos

---
+++ listas.js
@@ -0,0 +1,31 @@
+console.log('trabalhando com listas');
+
+const listasDeDestinos = new Array(
+    `Salvador`, 
+    `Rio de Janeiro`,
+    `Gramado`
+);
+
+console.log(listasDeDestinos);
+
+listasDeDestinos.push(`São Paulo`);
+
+console.log(listasDeDestinos);
+
+listasDeDestinos.splice(1,1);
+console.log(listasDeDestinos);
+
+console.log(listasDeDestinos[2], listasDeDestinos[1]);
+
+
+// O que aprendemos nessa aula:
+
+// Criação de Arrays;
+
+// Inserção de itens em um array;
+
+// Array splice;
+
+// Palavras reservadas;
+
+// Indices do array;
