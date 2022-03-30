935b2686ad2d1b79e24af61791776862346a9040
Liliane Soares <78764415+soareslil@users.noreply.github.com>
2022-03-30 14:41:01 -0300

    tipos  de variaveis

---
+++ variaveis.js
@@ -0,0 +1,19 @@
+console.log('trabalhando com atribuição de variaveis');
+
+
+const nome = 'liliane';
+const sobrenome = 'soares';
+
+console.log(nome, sobrenome);
+console.log(`meu nome é ${nome} ${sobrenome}`);
+
+nomeSobrenome = nome + sobrenome;
+console.log(nomeSobrenome);
+
+
+// Const e let
+// Case Sensitive
+// Adicionando comentários
+// parseInt e parseFloat
+// Usando números com casa decimal
+// Atribuição de variáveis
