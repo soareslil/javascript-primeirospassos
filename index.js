e4afde801f79e1c33a72c759252bba64e6906b13
Liliane Soares <78764415+soareslil@users.noreply.github.com>
2022-03-30 09:03:35 -0300

    finalizando introducao a linguagem

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 console.log('Hello World');
+
+
+
+
+// aqui retornou um texto que eu digitei
+// const idade = 25;
+// console.log('idade');
+
+
+// aqui retorna o valor da variavel idade
+const idade = 25;
+console.log(idade);
+
+
+
+const idadeSomada = idade+2;
+console.log(idadeSomada);
